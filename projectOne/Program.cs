94543f55a6fd9e14c1c8401e7f739feba8caba04
berale <b.y.berale@gmail.com>
2025-05-13 12:43:11 +0300

    the extenal functions except the director

--- projectOne/Program.cs
+++ projectOne/Program.cs
@@ -10,41 +10,96 @@ namespace seriesProject
     internal class Program
     {
         // data
-        
-
-        string Start()
-        {
-           
-            // מתחילה את התוכנית - קולטת קלט ושולחת הלאה
-        }
+        List<int> series = new List<int>();
+        Dictionary<string, string> menu = new Dictionary<string, string>();
 
         // פונקציות עזר
 
+        string Input(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            return input;
+        }
+
         void ErroreMeassge(string s)
         {
-           
+
         }
+
+
+        string Start()
+        {
+
+            // מתחילה את התוכנית - קולטת קלט ושולחת הלאה
+
+            string input = Input("enter a series of numbers" +
+                "(Numbers separated by spaces)");
+            
+            validation(input);
+        }
+
+        
 
         // פונקציות חיצוניות
 
-        List<string> validation(string input)
+        void validation(string input)
         {
             /* תיאור: מוודאת שהקלט תקין
-             * פירוט:
-             * data:
-             * 1 = משתנה בולייאן האם תקין
-             * 2 = משתנה מספר שמחזיק את אורך הרשימה 
-             * 
-             * פעולות:
-             * 1 הופכת לרשימה לפי הרווחים
-             * 2 מוודאת שהאורך תקין
-             * או שווה ל3
-             * 3 = רצה על הרשימה בלולאת ווייל עד אורך הרשימה
-             *   וכל עוד הבולייאן טרו
-             *   הבולייאן משתנה אם קיים מינוס או זהו איננו מספר
-             * 4 = אם המשתנה תקין שולחת הלאה, אם לא
-             *  קוראת לפונקציה הראשונה
-            */
+            * פירוט:
+            * data:
+            * 1 = משתנה בולייאן האם תקין
+            * 2 = משתנה מספר שמחזיק את אורך הרשימה 
+            * 
+            * פעולות:
+            * 1 הופכת לרשימה לפי הרווחים
+            * 2 מוודאת שהאורך תקין
+            * או שווה ל3
+            * 3 = רצה על הרשימה בלולאת ווייל עד אורך הרשימה
+            *   וכל עוד הבולייאן טרו
+            *   הבולייאן משתנה אם קיים מינוס או זהו איננו מספר
+            * 4 = אם המשתנה תקין שולחת הלאה, אם לא
+            *  קוראת לפונקציה הראשונה
+           */
+
+            List<string> seriesString = new List<string>(input.Split(" "));
+            bool proper = true;
+            int len = Len(seriesString);
+            int i = 0;
+
+            if (len < 3)
+            {
+                proper = false;
+
+            }
+            else
+            {
+                while (i < len && proper)
+                {
+                    foreach (char c in seriesString[i])
+                    {
+                        if (!(char.IsDigit(c)) || (c == '-'))
+                        {
+                            proper = false;
+                            break;
+                        }
+                    }
+                    i++;
+                }
+            }
+            if (proper)
+            {
+                ListOfInt(seriesString);
+            }
+            else
+            {
+                ErroreMeassge("");
+                Start();
+            }
+
+
+
+           
         }
 
         void ListOfInt(List<string> properInput)
@@ -53,6 +108,14 @@ namespace seriesProject
              * ומעדכנת את "TheSeries"
              * 
              */
+
+            foreach(string number in properInput)
+            {
+                series.Add(int.Parse(number));
+            }
+            
+
+
         }
 
        
@@ -65,8 +128,12 @@ namespace seriesProject
              * מילון התפריט
              * פעולה:
              * רצה על המילון "menu" ומדפיסה למשתמש
-             * וקורת לפונקציה הבאה
+             * וקוראת לפונקציה הבאה
              */ 
+            foreach (KeyValuePair <string, string> pair in menu)
+                {
+                    Console.WriteLine($"{pair.Key}. {pair.Value}");
+                }
         }
 
         void Manage()
@@ -84,6 +151,7 @@ namespace seriesProject
              * בסוף הפונקציה קוראת לפונקציית התפריט
              * אלא אם אקסיט
              */
+
 
         }
 

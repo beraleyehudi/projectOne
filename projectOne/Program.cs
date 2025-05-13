54db1de32ef9316b17f953afd873e795848c3216
berale <b.y.berale@gmail.com>
2025-05-13 11:54:07 +0300

    Functions signatures

--- projectOne/Program.cs
+++ projectOne/Program.cs
@@ -1,2 +1,141 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seriesProject
+{
+    internal class Program
+    {
+        // data
+        
+
+        string Start()
+        {
+           
+            // מתחילה את התוכנית - קולטת קלט ושולחת הלאה
+        }
+
+        // פונקציות עזר
+
+        void ErroreMeassge(string s)
+        {
+           
+        }
+
+        // פונקציות חיצוניות
+
+        List<string> validation(string input)
+        {
+            /* תיאור: מוודאת שהקלט תקין
+             * פירוט:
+             * data:
+             * 1 = משתנה בולייאן האם תקין
+             * 2 = משתנה מספר שמחזיק את אורך הרשימה 
+             * 
+             * פעולות:
+             * 1 הופכת לרשימה לפי הרווחים
+             * 2 מוודאת שהאורך תקין
+             * או שווה ל3
+             * 3 = רצה על הרשימה בלולאת ווייל עד אורך הרשימה
+             *   וכל עוד הבולייאן טרו
+             *   הבולייאן משתנה אם קיים מינוס או זהו איננו מספר
+             * 4 = אם המשתנה תקין שולחת הלאה, אם לא
+             *  קוראת לפונקציה הראשונה
+            */
+        }
+
+        void ListOfInt(List<string> properInput)
+        {
+            /* תיאור: הופכת לרשימה של מספרים 
+             * ומעדכנת את "TheSeries"
+             * 
+             */
+        }
+
+       
+   
+
+        void Menu()
+        {
+            /* תיאור: מציגה את התפריט
+             * data:
+             * מילון התפריט
+             * פעולה:
+             * רצה על המילון "menu" ומדפיסה למשתמש
+             * וקורת לפונקציה הבאה
+             */ 
+        }
+
+        void Manage()
+        {
+            /* תיאור: מנהלת ושולחת לכל פונקציה 
+             * לפי בחירת המשתמש
+             * data:
+             * theSirius
+             * bool exit = false
+             * 
+             * פעולות:
+             * קולטת מהמשתמש
+             * אם הקליטה אינה טובה מציגה הודעת 
+             * שגיאה
+             * בסוף הפונקציה קוראת לפונקציית התפריט
+             * אלא אם אקסיט
+             */
+
+        }
+
+        // פונקציות פנימיות
+
+       void Print()
+        {
+
+        }
+
+       void PrintReverse()
+        {
+
+        }
+
+        void Sort()
+        {
+
+        }
+
+        void Max()
+        {
+
+        }
+
+        void Min()
+        {
+
+        }
+
+        void Avrerage()
+        {
+
+        }
+         
+        void Len()
+        {
+
+        }
+
+        void Sum()
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Start();
+
+
+        }
+    }
+}
+

b5fc6952b0d6998ec67f33cd81f970067cb1f355
berale <b.y.berale@gmail.com>
2025-05-13 15:56:34 +0300

    fulfilling the functions - flow test

--- projectOne/Program.cs
+++ projectOne/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
@@ -11,7 +12,24 @@ namespace seriesProject
     {
         // data
         List<int> series = new List<int>();
-        Dictionary<string, string> menu = new Dictionary<string, string>();
+        List<int> sortedList = new List<int>();
+        List<int> emptyList = new List<int>() { 0};
+        bool sorted = false;
+        int theLen; 
+        Dictionary<string, string> menuData = new Dictionary<string, string>()
+        {
+            {"1", "Switch series"},
+            {"2",  "print"},
+            {"3", "print reverse" },
+            {"4", "print sorted series" },
+            {"5",  "find max number"},
+            {"6", "find min number" },
+            {"7", "average" },
+            {"8", "lengh" },
+            {"9", "sum" },
+            {"10", "exit from the program" }
+        };
+        
 
         // פונקציות עזר
 
@@ -22,17 +40,18 @@ namespace seriesProject
             return input;
         }
 
-        void ErroreMeassge(string s)
+        void ErroreMeassge(string specificlyMeassage)
         {
+            Console.WriteLine($"Incorrect input \n Please note that {specificlyMeassage}");
 
         }
 
 
-        string Start()
+        void Start()
         {
 
             // מתחילה את התוכנית - קולטת קלט ושולחת הלאה
-
+            sorted = false;
             string input = Input("enter a series of numbers" +
                 "(Numbers separated by spaces)");
             
@@ -64,17 +83,17 @@ namespace seriesProject
 
             List<string> seriesString = new List<string>(input.Split(" "));
             bool proper = true;
-            int len = Len(seriesString);
+            theLen = 8;
             int i = 0;
 
-            if (len < 3)
+            if (theLen < 3)
             {
                 proper = false;
 
             }
             else
             {
-                while (i < len && proper)
+                while (i < theLen && proper)
                 {
                     foreach (char c in seriesString[i])
                     {
@@ -93,7 +112,7 @@ namespace seriesProject
             }
             else
             {
-                ErroreMeassge("");
+                ErroreMeassge("You are entering numbers separated by a space.");
                 Start();
             }
 
@@ -113,6 +132,7 @@ namespace seriesProject
             {
                 series.Add(int.Parse(number));
             }
+            Menu();
             
 
 
@@ -130,14 +150,16 @@ namespace seriesProject
              * רצה על המילון "menu" ומדפיסה למשתמש
              * וקוראת לפונקציה הבאה
              */ 
-            foreach (KeyValuePair <string, string> pair in menu)
+            foreach (KeyValuePair <string, string> pair in menuData)
                 {
                     Console.WriteLine($"{pair.Key}. {pair.Value}");
                 }
+            Manage();
         }
 
         void Manage()
         {
+
             /* תיאור: מנהלת ושולחת לכל פונקציה 
              * לפי בחירת המשתמש
              * data:
@@ -151,56 +173,195 @@ namespace seriesProject
              * בסוף הפונקציה קוראת לפונקציית התפריט
              * אלא אם אקסיט
              */
+            string input = Input("type your chiose \n");
+            List<int> theObject = new List<int>();
+            bool exit = false;
+
+            switch (input)
+            {
+                case "1":
+                    Start();
+                    break;
+
+                case "2":
+                    Print(series);
+                    break;
+
+                case "3":
+                    PrintReverse();
+                    break;
+
+                case "4":
+                    theObject = Sorts();
+                    break;
+
+                case "5":
+                    theObject = Max();
+                    break;
+
+                case "6":
+                    theObject = Min();
+                    break;
+
+                case "7":
+                    theObject = Avrerage();
+                    break;
+
+                case "8":
+                    theObject = Len();
+                    break;
+
+                case "9":
+                    theObject = Sum(); 
+                    break;
+
+                case "10":
+                    exit = true;
+                    break;
+
+                default:
+                ErroreMeassge("Enter a number between 1 and 10.");
+                Menu();
+                break;
+            }
+            if (!exit)
+            {
+                if (input != "2")
+                {
+                    Print(theObject);
+
+                }
+                Menu();
+            }
+            else
+            {
+                Console.WriteLine("היה כיף אח שלי");
+            }
+
 
 
         }
 
         // פונקציות פנימיות
 
-       void Print()
+       void Print(List<int> theObject)
         {
+            foreach (int i in theObject)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n");
 
         }
 
        void PrintReverse()
         {
+            for (int i = theLen - 1; i >= 0; i--)
+            {
+                Console.Write(series[i] + " ");
+            }
+            Console.WriteLine("\n");
 
         }
 
-        void Sort()
+        List<int> Sorts()
         {
+            if (!sorted)
+            {
+                sorted = true;
+                sortedList = new List<int>(series);
+                sortedList.Sort();
+            }
+            return sortedList;
 
         }
 
-        void Max()
+        List<int> Max()
         {
+            int max = series[0];
+            if (sorted)
+            {
+                emptyList[0] = sortedList[theLen - 1];
+                
+            }
+            else
+            {
+                foreach (int i in series)
+                {
+                    if (i > max)
+                    {
+                        max = i;
+                    }
+                }
+
+                emptyList[0] = max;
+            }
+            return emptyList;
 
         }
 
-        void Min()
+        List<int> Min()
         {
+            int min = series[0];
+            if (sorted)
+            {
+                emptyList[0] = sortedList[0];
+
+            }
+            else
+            {
+                foreach (int i in series)
+                {
+                    if (i < min)
+                    {
+                         min = i;
+
+                    }
+                }
+                emptyList[0] = min;
+            }
+            return emptyList;
+
 
         }
 
-        void Avrerage()
+        List<int> Avrerage()
         {
+            int average =  Sum()[0] / Len()[0];
+            emptyList[0] = average;
+            return emptyList;
 
         }
-         
-        void Len()
+
+        List<int> Len()
         {
+            int len = 0;
+            foreach (int i in series)
+            {
+                len++;
+            }
+            emptyList[0] = len;
+            return emptyList;
 
         }
 
-        void Sum()
+        List<int> Sum()
         {
+            int sum = 0;
+            foreach (int i in series)
+            {
+                sum += i;
+            }
+            emptyList[0] = sum;
+            return emptyList;
 
         }
 
 
         static void Main(string[] args)
         {
-            Start();
+            new Program().Start();
+            
 
 
         }
